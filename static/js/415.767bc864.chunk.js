c781a1e922f11fb174db8f98780c54582ffb8a9b
PatrycjaWiza <95050810+PatrycjaWiza@users.noreply.github.com>
2022-10-16 21:16:39 +0000

    Deploying to gh-pages from @ PatrycjaWiza/goit-react-hw-08-phonebook@84da06da0de28b221c00d7d17f4862d12405c73b 🚀

---
+++ static/js/415.767bc864.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,o,n){n.r(o),n.d(o,{default:function(){return t}});var r=n(3329);function t(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h1",{children:"Welcome to your personal PhonebookApp!"})})}}}]);
+//# sourceMappingURL=415.767bc864.chunk.js.map
