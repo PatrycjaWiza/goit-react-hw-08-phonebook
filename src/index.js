2779c605bd04be0c7031cfa9e8bddd168e46619f
PatrycjaWiza <95050810+PatrycjaWiza@users.noreply.github.com>
2022-10-16 23:13:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter /*basename="/localhost:3000"*/>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter /*basename="/localhost:3000"*/>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
